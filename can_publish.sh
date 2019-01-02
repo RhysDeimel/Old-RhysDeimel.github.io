643e5a093e964e879aee1e3b271edec6c07fc6d3
RhysDeimel <rhys.deimel@nirovision.com>
2019-01-03 01:16:52 +1100

    testing can_publish

--- can_publish.sh
+++ can_publish.sh
@@ -4,27 +4,16 @@ today=$(date +%s)
 for filename in content/drafts/*.md; do
     [ -e "$filename" ] || continue
 
-    # check files that contain "Status: ready"
     if grep -q "Status: ready" $filename; then
-        echo "$filename contains ready"
-        post_date=$(grep "Date" $filename | cut -d' ' -f2 | date -d +%s)
+        echo "$(basename $filename) is finalised"
+        post_date=$(grep Date $filename | cut -d' ' -f2 | date +%s)
 
         if [ "$today" -ge "$post_date" ]; then
-            echo "$filename should be published"
+            echo "$(basename $filename) should be published"
+            sed -i '' 's/Status: ready/Status: published/' $filename
+            mv $filename content/posts/$(date +%Y)/$(basename $filename)
+        else
+            echo "Publish date has not been reached, skipping."
         fi
-
     fi
-    
-        # check date is greater than or equal to today
-            # change status to "published"
-            # move to posts/(folder that matches year)
 done
-
-
-# todate=$(date -d 2013-07-18 +%s)
-# cond=$(date -d 2014-08-19 +%s)
-
-# if [ $todate -ge $cond ];
-# then
-#     break
-# fi  
