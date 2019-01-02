339d7892380b70c377f7e718b1937b1489fdc3d2
RhysDeimel <rhys.deimel@nirovision.com>
2019-01-02 17:17:09 +1100

    partial publish script completion

--- can_publish.sh
+++ can_publish.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
+today=$(date +%s)
 
 for filename in content/drafts/*.md; do
     [ -e "$filename" ] || continue
-    echo "should do some stuff here"
+
     # check files that contain "Status: ready"
+    if grep -q "Status: ready" $filename; then
+        echo "$filename contains ready"
+        post_date=$(grep "Date" $filename | cut -d' ' -f2 | date -d +%s)
+
+        if [ "$today" -ge "$post_date" ]; then
+            echo "$filename should be published"
+        fi
+
+    fi
+    
         # check date is greater than or equal to today
             # change status to "published"
             # move to posts/(folder that matches year)
