4b59c93a000247821c0d1be52344a41397e8cee8
RhysDeimel <rhys.deimel@nirovision.com>
2019-01-03 16:11:54 +1100

    testing push script

--- push.sh
+++ push.sh
@@ -6,15 +6,17 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b src
-  git add content
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git status
+  # git checkout -b src
+  # git add content
+  # git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   # git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
   # git push --quiet --set-upstream origin-pages gh-pages 
-  git push
+  # git push
+  echo "would push here"
 }
 
 setup_git
