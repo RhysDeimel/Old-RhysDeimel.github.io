f2c520d6c1c9a7bc2df7d2934c56f2c061da2b65
RhysDeimel <rhys.deimel@nirovision.com>
2019-01-03 15:46:55 +1100

    fixed date issue

--- can_publish.sh
+++ can_publish.sh
@@ -6,12 +6,11 @@ for filename in content/drafts/*.md; do
 
     if grep -q "Status: ready" $filename; then
         echo "$(basename $filename) is finalised"
-        post_date=$(grep Date $filename | cut -d' ' -f2 | date +%s)
+        post_date=$(grep Date $filename | cut -d' ' -f2)
+        post_date=$(date -d $post_date +%s)
 
         if [ "$today" -ge "$post_date" ]; then
             echo "$(basename $filename) should be published"
-            echo "today is $today"
-            echo "post_date is $post_date"
 
             sed -i 's/Status: ready/Status: published/' $filename
             mkdir -p content/posts/$(date +%Y)/
