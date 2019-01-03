4a79917ff5b445c032126ed973cd258d3cd74baa
RhysDeimel <rhys.deimel@nirovision.com>
2019-01-03 21:01:47 +1100

    this might work

--- push.sh
+++ push.sh
@@ -1,25 +1,11 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
 
-commit_website_files() {
-  # git status
-  # git checkout -b src
-  git add content
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
+echo "Adding any changes in content"
+git add content
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/RhysDeimel/RhysDeimel.github.io.git > /dev/null 2>&1
-  # git push --quiet --set-upstream origin-pages gh-pages 
-  # git push
-  echo "would push here"
-  git push origin HEAD:src
-}
-
-setup_git
-commit_website_files
-upload_files
+echo "Pushing any changes in content"
+git push -u https://RhysDeimel:$GITHUB_TOKEN@github.com/RhysDeimel/RhysDeimel.github.io.git/ HEAD:src
