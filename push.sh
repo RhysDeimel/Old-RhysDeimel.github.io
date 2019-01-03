43cc67793baf07881a6c686fe7216040e3c37f37
RhysDeimel <rhys.deimel@nirovision.com>
2019-01-03 21:08:03 +1100

    looks like it works. Adjusting position in travis.yml

--- push.sh
+++ push.sh
@@ -3,9 +3,9 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-echo "Adding any changes in content"
+echo "Adding any changes in content:"
 git add content
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-echo "Pushing any changes in content"
+echo "Pushing any changes in content:"
 git push -u https://RhysDeimel:$GITHUB_TOKEN@github.com/RhysDeimel/RhysDeimel.github.io.git/ HEAD:src
