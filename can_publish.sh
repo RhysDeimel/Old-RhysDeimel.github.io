24820a22ede8de22cde706facc6bf2d8f441650a
RhysDeimel <rhys.deimel@nirovision.com>
2019-01-07 12:10:07 +1100

    something funky is going on...

--- can_publish.sh
+++ can_publish.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
+
 today=$(date +%s)
 
 echo "Checking to see if anything needs publishing"
@@ -8,7 +10,7 @@ for filename in content/drafts/*.md; do
     if grep -q "Status: ready" $filename; then
         echo "$(basename $filename) is finalised"
         post_date=$(grep Date $filename | cut -d' ' -f2)
-        post_date=$(date -d $post_date +%s)
+        post_date=$(date -d "$post_date" +%s)
 
         if [ "$today" -ge "$post_date" ]; then
             echo "$(basename $filename) should be published"
